e919c80a54bba38573e8d72f45b2058c3014b8a9
brave187 <166106241+brave187@users.noreply.github.com>
2026-06-09 14:44:46 +0200

    Update rss_aggregator.py

--- rss_aggregator.py
+++ rss_aggregator.py
@@ -44,6 +44,14 @@ rss_feed_urls = [
     "https://techcommunity.microsoft.com/t5/s/gxcuf89792/rss/board?board.id=securitycopilotblog",
     "https://techcommunity.microsoft.com/t5/s/gxcuf89792/rss/board?board.id=microsoft-purview-blog",
     "https://www.microsoft.com/en-us/security/blog/topic/threat-intelligence/feed/",
+    "https://techcommunity.microsoft.com/t5/s/gxcuf89792/rss/board?board.id=exchange",
+    "https://techcommunity.microsoft.com/t5/s/gxcuf89792/rss/board?board.id=IntuneCustomerSuccess",
+    "https://techcommunity.microsoft.com/t5/s/gxcuf89792/rss/board?board.id=microsoft_365blog",
+    "https://techcommunity.microsoft.com/t5/s/gxcuf89792/rss/board?board.id=MicrosoftTeamsBlog",
+    "https://techcommunity.microsoft.com/t5/s/gxcuf89792/rss/board?board.id=microsoft-entra-blog",
+    "https://techcommunity.microsoft.com/t5/s/gxcuf89792/rss/board?board.id=azuretoolsblog",
+    "https://techcommunity.microsoft.com/t5/s/gxcuf89792/rss/board?board.id=microsoft-security-baselines",
+    "https://techcommunity.microsoft.com/t5/s/gxcuf89792/rss/board?board.id=windows-itpro-blog",    
 ]
 
 # Set the output file name and state file
